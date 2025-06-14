2c425a2d7897e965ef70612ebd59da6196f4aba9
Marco <marcop@mailbox.org>
2025-06-14 10:23:45 -0700

    Add GoogleTagManager to base layout
    Note: Set GOOGLE_TAG_MANAGER_ID in your environment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { LanguageProvider } from '@/contexts/LanguageContext';
 import './globals.css';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -12,6 +13,10 @@ const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
 });
+
+// Google Tag Manager ID
+const GTM_ID = process.env
+  .GOOGLE_TAG_MANAGER_ID as string;
 
 // 메타데이터는 서버 컴포넌트에서만 사용 가능
 export const metadata: Metadata = {
@@ -28,6 +33,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId={`${GTM_ID}`} />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
