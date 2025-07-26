f92b66ff35fb093c6ef0ae90c40755d5c5862aee
Heeju Jo <cuteprobe@gmail.com>
2025-07-26 09:05:38 +0900

    updated partner logos in Hero

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -18,22 +18,23 @@ const Hero = () => {
         <div className="absolute bottom-0 left-0 w-1/3 h-1/3 bg-fitculator-secondary opacity-10 blur-[100px] rounded-full"></div>
       </div>
 
-      <div className="container mx-auto px-4 relative z-10">
+      <div className="container mt-18 mx-auto px-4 relative z-10">
         <FadeUp className="text-center max-w-4xl mx-auto">
           {/* 로고 */}
-          <div className="mb-8 flex justify-center">
+          {/* <div className="mb-8 flex justify-center">
             <h1 className="text-4xl md:text-6xl font-bold gradient-text">
               Fitculator
             </h1>
-          </div>
+          </div> */}
 
-          {/* 메인 헤드라인 */}
-          <h2 className="text-3xl md:text-5xl font-bold mb-6">
-            {getTextByLanguage(translations.hero.title, language)}
-          </h2>
-          <h3 className="text-xl md:text-3xl font-semibold mb-8 text-gray-300">
+          <h3 className="text-lg md:text-xl font-normal mb-6 text-gray-300">
             {getTextByLanguage(translations.hero.subtitle, language)}
           </h3>
+
+          {/* 메인 헤드라인 */}
+          <h2 className="text-3xl md:text-7xl font-bold mb-6">
+            {getTextByLanguage(translations.hero.title, language)}
+          </h2>
 
           {/* 서브 헤드라인 */}
           <p className="text-lg md:text-xl text-gray-300 mb-10 max-w-3xl mx-auto">
@@ -51,19 +52,19 @@ const Hero = () => {
           </div>
 
           {/* 파트너십 */}
-          <div className="mt-8 flex items-center justify-center gap-3">
+          <div className="mt-8 flex items-baseline justify-center gap-3">
             <p className="text-gray-300">Officially partnered with</p>
             <Image
               src="/Garmin Logo Without Delta-white-low-res.png"
               alt="Garmin Logo"
-              width={100}
-              height={30}
+              width={120}
+              height={36}
               className="object-contain"
             />
             <Image
               src="/Amazfit_logo_white.png"
               alt="Amazfit Logo"
-              width={90}
+              width={100}
               height={27}
               className="object-contain"
             />
